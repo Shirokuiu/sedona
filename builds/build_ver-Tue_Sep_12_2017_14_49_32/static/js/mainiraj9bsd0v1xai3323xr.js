5dee93be266729f5d2d12c81d02bd086f600ccad
Pavel <shirokuiuna@mail.ru>
2017-09-12 14:50:17 +0300

    upload realese build

---
+++ builds/build_ver-Tue_Sep_12_2017_14_49_32/static/js/mainiraj9bsd0v1xai3323xr.js
@@ -0,0 +1,29 @@
+$(function() {
+  var mainNavButton = $('#mainNavButton');
+  var mainNavButtonClose = $('#mainNavButtonClose');
+  var mainNavBlockWrap = $('#mainNavBlockWrap');
+  var mainNavList = $('#mainNavBlockWrap #mainNavList');
+  var orderButton = $('#orderButton');
+  var orderPopup = $('#orderPopup');
+  var aboutBlockHeadline = $('#aboutBlockHeadline').text();
+  
+  mainNavButton.click(function() {
+    mainNavButtonClose.addClass('main-nav__button-close--vissible');
+    mainNavBlockWrap.toggleClass('main-nav__blockWrap--padding-vissible');
+    mainNavList.toggleClass('main-nav__list--vissible');
+  });
+  
+  mainNavButtonClose.click(function () {
+    $(this).removeClass('main-nav__button-close--vissible');
+    mainNavBlockWrap.removeClass('main-nav__blockWrap--padding-vissible');
+    mainNavList.removeClass('main-nav__list--vissible');
+  });
+  
+  orderButton.click(function () {
+    orderPopup.slideToggle('1000');
+  });
+});
+
+
+
+
