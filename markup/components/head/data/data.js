cc474089e9911d630ba91932fc0f7551866cfd17
Pavel <shirokuiuna@mail.ru>
2017-09-12 14:47:10 +0300

    upload sedona project

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,14 @@
+head: {
+    defaults: {
+        title: 'Главная страница',
+        useSocialMetaTags: true
+    },
+    'photo': {
+      title: 'Фото и видео',
+      useSocialMetaTags: true
+    },
+    'form': {
+      title: 'Форма отзыва',
+      useSocialMetaTags: true
+    }
+}
